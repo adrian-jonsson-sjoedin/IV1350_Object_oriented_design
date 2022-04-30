c8af724c9ec67767447785efed4f0a417ca4f831
Adrian Jonsson Sjödin <adrian.jonsson@outlook.com>
2022-04-30 17:37:14 +0200

    ExternalInventory Class done

--- POS/src/se/kth/iv1350/pos/integration/ExternalInventorySystem.java
+++ POS/src/se/kth/iv1350/pos/integration/ExternalInventorySystem.java
@@ -13,6 +13,48 @@ public class ExternalInventorySystem {
      * Package private constructor that creates the placeholder inventory.
      */
     ExternalInventorySystem() {
+        inventory.add(new ItemDTO("Apple, aroma", 3006, 5, 5));
+        inventory.add(new ItemDTO("Cinnamon bun", 6880, 7.95, 5));
+        inventory.add(new ItemDTO("Red paprika", 4680, 10.95, 5));
+        inventory.add(new ItemDTO("Carlsberg 330ml", 1111, 11.95, 10));
+        inventory.add(new ItemDTO("Tampongs", 2222, 26.95, 2));
+    }
 
+    /**
+     * This method takes an eanCode and checks if an item with that eanCode is already in the inventory.
+     *
+     * @param eanCode Used to search for the item in the list.
+     * @return True if an item with the parameter is already in the inventory, otherwise returns false.
+     */
+    public boolean checkIfItemExists(int eanCode) {
+        for (ItemDTO itemDTO : inventory) {
+            if (itemDTO.getEanCode() == eanCode) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Finds the item in the inventory and retrieves a reference to it.
+     *
+     * @param eanCode Identifies which item that should be retrieved.
+     * @return Returns a reference to that item.
+     */
+    public ItemDTO retrieveItem(int eanCode) {
+        ItemDTO itemToRetrieve;
+        if (checkIfItemExists(eanCode)) {
+            for (ItemDTO itemDTO : inventory) {
+                if (itemDTO.getEanCode() == eanCode) {
+                    itemToRetrieve = itemDTO;
+                    return itemToRetrieve;
+                }
+            }
+        }
+        return null;
+    }
+
+    public List<ItemDTO> getInventory() {
+        return inventory;
     }
 }
--- POS/src/se/kth/iv1350/pos/integration/ItemDTO.java
+++ POS/src/se/kth/iv1350/pos/integration/ItemDTO.java
@@ -1,4 +1,41 @@
 package se.kth.iv1350.pos.integration;
 
+/**
+ * DTO for the item
+ */
 public class ItemDTO {
+    private String itemName;
+    private int eanCode;
+    private double itemPrice;
+    private double vatRate;
+
+    /**
+     * Created an item with the specified parameters.
+     * @param itemName Describes the item.
+     * @param eanCode Unique identifier for the item.
+     * @param itemPrice The price for the item.
+     * @param vatRate The items VAT rate.
+     */
+    ItemDTO(String itemName, int eanCode, double itemPrice, double vatRate) {
+        this.itemName = itemName;
+        this.eanCode = eanCode;
+        this.itemPrice = itemPrice;
+        this.vatRate = vatRate;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public int getEanCode() {
+        return eanCode;
+    }
+
+    public double getItemPrice() {
+        return itemPrice;
+    }
+
+    public double getVatRate() {
+        return vatRate;
+    }
 }
