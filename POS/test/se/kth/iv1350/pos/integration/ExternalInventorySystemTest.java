76e5fa2ee88328bd2de2b19ef3183a4bcd37da31
Adrian Jonsson Sjödin <adrian.jonsson@outlook.com>
2022-05-01 14:49:23 +0200

    Set up the test structure. Going to start testing the class Sale and ExternalInventorySystem

---
+++ POS/test/se/kth/iv1350/pos/integration/ExternalInventorySystemTest.java
@@ -0,0 +1,17 @@
+package se.kth.iv1350.pos.integration;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExternalInventorySystemTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
