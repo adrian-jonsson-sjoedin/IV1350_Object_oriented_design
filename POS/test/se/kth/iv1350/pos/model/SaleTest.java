2308b69d412c4c6777a7af3d3db4f571dc9ee2cc
Adrian Jonsson Sjödin <adrian.jonsson@outlook.com>
2022-05-02 19:30:50 +0200

    Sale Test class done for the current implementation of sale

--- POS/test/se/kth/iv1350/pos/model/SaleTest.java
+++ POS/test/se/kth/iv1350/pos/model/SaleTest.java
@@ -13,11 +13,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class SaleTest {
     private Sale sale;
     private ItemDTO itemDummy;
+    private ItemDTO anotherItemDummy;
 
     @BeforeEach
     void setUp() {
         sale = new Sale();
         itemDummy = new ItemDTO("dummy", 9999, 99.99, 99);
+        anotherItemDummy = new ItemDTO("Another dummy", 8888, 89.99, 89);
         sale.addItemToBasket(itemDummy, 1);
     }
 
@@ -33,5 +35,39 @@ class SaleTest {
         int expected = 2;
         int actual = sale.getBasket().get(0).getQuantity();
         assertEquals(expected,actual, "Quantity did not update correctly");
+    }
+    @Test
+    public void testAddNewItem() {
+        sale.addItemToBasket(anotherItemDummy,1);
+        int expectedBasketSize = 2;
+        int actualBasketSize = sale.getBasket().size();
+        assertEquals(expectedBasketSize,actualBasketSize,"Item was not added to basket");
+    }
+    @Test
+    public void testCanRetrieveItemName() {
+        String expected ="dummy";
+        String actual = sale.getBasket().get(0).getItemName();
+        assertEquals(expected,actual,"Item name did not match");
+    }
+
+    @Test
+    public void testCanRetrieveItemPrice() {
+        double expected = 99.99;
+        double actual = sale.getBasket().get(0).getItemPrice();
+        assertEquals(expected,actual,"Item price did not match");
+    }
+
+    @Test
+    public void testCanRetrieveItemVatRate() {
+        double expected = 99;
+        double actual = sale.getBasket().get(0).getItemVatRate();
+        assertEquals(expected,actual,"Item VAT rate did not match");
+    }
+
+    @Test
+    public void testCanRetrieveItemEanCode() {
+        double expected = 9999;
+        double actual = sale.getBasket().get(0).getItemEanCode();
+        assertEquals(expected,actual,"Item's EAN code matched");
     }
 }
