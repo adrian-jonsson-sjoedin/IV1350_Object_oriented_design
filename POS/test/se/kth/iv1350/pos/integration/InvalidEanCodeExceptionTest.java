5512e886dff2b74bf58a8a0771736a4fe6b5e0e1
Adrian Jonsson Sjödin <adrian.jonsson@outlook.com>
2022-05-19 17:03:43 +0200

    Testing of InventoryDBUnresponsiveException done

---
+++ POS/test/se/kth/iv1350/pos/integration/InvalidEanCodeExceptionTest.java
@@ -0,0 +1,7 @@
+package se.kth.iv1350.pos.integration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InvalidEanCodeExceptionTest {
+
+}
---
+++ POS/test/se/kth/iv1350/pos/integration/InventoryDBUnresponsiveExceptionTest.java
@@ -0,0 +1,36 @@
+package se.kth.iv1350.pos.integration;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InventoryDBUnresponsiveExceptionTest {
+    ExternalInventorySystem inventory;
+
+    @BeforeEach
+    void setUp() {
+        inventory = new ExternalInventorySystem();
+    }
+
+    @AfterEach
+    void tearDown() {
+        inventory = null;
+    }
+
+    @Test
+    public void testExceptionShouldNotBeThrown() {
+        int eanCode = 6880;
+        Assertions.assertDoesNotThrow(() -> inventory.retrieveItem(eanCode), "An exception was thrown," +
+                "which it should not");
+    }
+
+    @Test
+    public void testExceptionIsThrown() {
+        int dbFailureEanCode = 6969;
+        assertThrows(InventoryDBUnresponsiveException.class, () -> inventory.retrieveItem(dbFailureEanCode),
+                "An exception was not thrown");
+    }
+}
