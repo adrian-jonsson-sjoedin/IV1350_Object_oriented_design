ddf18b2abdc3cc72ff2280ca1fbd01b72af1d9b5
Adrian Jonsson Sjödin <adrian.jonsson@outlook.com>
2022-05-19 17:10:24 +0200

    Tested InvalidEanCodeException

--- POS/test/se/kth/iv1350/pos/integration/InvalidEanCodeExceptionTest.java
+++ POS/test/se/kth/iv1350/pos/integration/InvalidEanCodeExceptionTest.java
@@ -1,7 +1,34 @@
 package se.kth.iv1350.pos.integration;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class InvalidEanCodeExceptionTest {
+    ExternalInventorySystem inventory;
 
+    @BeforeEach
+    void setUp() {
+        inventory = new ExternalInventorySystem();
+    }
+
+    @AfterEach
+    void tearDown() {
+        inventory= null;
+    }
+
+    @Test
+    public void testItemExists() {
+        int existingItemEanCode = 6880;
+        assertDoesNotThrow(() -> inventory.retrieveItem(existingItemEanCode), "Throws an unexpected exception");
+    }
+
+    @Test
+    public void testItemDoesNotExistException() {
+        int invalidEanCode = 1;
+        assertThrows(InvalidEanCodeException.class, () -> inventory.retrieveItem(invalidEanCode),
+                "An exception was not thrown");
+    }
 }
